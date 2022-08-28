80d93f67184aecde50dc236af6273f58cd4d0a4f
Hailey2022 <96413516+Hailey2022@users.noreply.github.com>
2022-08-28 14:12:46 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,13 @@
-/data/adb/clash -d /data/adb/meta &
-echo $! > ./clash.pid
+pid=$(cat ./clash.pid)
+if [ $pid == 0 ]
+then
+   /data/adb/clash -d /data/adb/meta &
+   echo $! > ./clash.pid
+elif [ $pid -gt 0 ]
+then
+   kill $pid
+   /data/adb/clash -d /data/adb/meta &
+   echo $! > ./clash.pid
+else
+   echo 0 > ./clash.pid
+fi
